e411568f9abf59dc4a602ded7747ff1a0cd443b2
Zeeshan <zeeshan@Zeeshans-MacBook-Air.local>
2023-09-19 23:32:10 +0530

    Adding Publisher code

--- src/main/java/com/zeeshan/publisher/controller/PublisherController.java
+++ src/main/java/com/zeeshan/publisher/controller/PublisherController.java
@@ -2,16 +2,38 @@ package com.zeeshan.publisher.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.Random;
 
 @RestController
 @Slf4j
 public class PublisherController {
 
 
-     @GetMapping("/")
-    public String health(){
-         return "Publisher is Healthy";
-     }
+    @GetMapping("/")
+    public String health() {
+        return "Publisher is Healthy";
+    }
+
+    @GetMapping("/even-numbers")
+    public Flux<Integer> getEvenData() {
+
+        Flux<Integer> evenNumbers = Flux
+                .range(0, 100)
+                .filter(x -> x % 2 == 0);
+        return evenNumbers;
+    }
+
+    @GetMapping("/odd-numbers")
+    public Flux<Integer> getOddData() {
+        Flux<Integer> oddNumbers = Flux
+                .range(0, 100)
+                .filter(x -> x % 2 > 0);
+        return oddNumbers;
+    }
+
+
 }
